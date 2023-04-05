c82837f1cd1f739af619d8c26fa9d55959671929
Jakkula <S560445@nwmissouri.edu>
2023-04-05 17:46:28 -0500

    chnages made

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var earphonesRouter = require('./routes/earphones');
+var boardRouter = require('./routes/users');
+var selectorRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/earphones', earphonesRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
